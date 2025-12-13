f0cbadc458f03d263fca96197532b941b86678a1
SantiagoGervasoni <cristiangervasoni89@gmail.com>
2025-12-13 18:08:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,31 +6,34 @@ function scrollToSection(id){
   const el = document.querySelector(id);
   if(el) el.scrollIntoView({behavior:'smooth', block:'start'});
 }
-const slides = document.querySelectorAll('.slide');
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
+const slides = document.querySelectorAll('.carrusel .slide');
+const prev = document.querySelector('.carrusel .prev');
+const next = document.querySelector('.carrusel .next');
 
 let index = 0;
 
 function mostrarSlide(i) {
-  slides.forEach(s => s.classList.remove('activo'));
-  slides[i].classList.add('activo');
+    slides.forEach(slide => slide.classList.remove('activo'));
+    slides[i].classList.add('activo');
 }
 
-prev.onclick = () => {
-  index = (index - 1 + slides.length) % slides.length;
-  mostrarSlide(index);
-};
+mostrarSlide(index);
 
-next.onclick = () => {
-  index = (index + 1) % slides.length;
-  mostrarSlide(index);
-};
+prev.addEventListener('click', () => {
+    index = (index - 1 + slides.length) % slides.length;
+    mostrarSlide(index);
+});
+
+next.addEventListener('click', () => {
+    index = (index + 1) % slides.length;
+    mostrarSlide(index);
+});
 
 setInterval(() => {
-  index = (index + 1) % slides.length;
-  mostrarSlide(index);
+    index = (index + 1) % slides.length;
+    mostrarSlide(index);
 }, 3000);
+
 
 function mostrarSeccion(id) {
     // Ocultar todas las secciones
@@ -50,6 +53,7 @@ function mostrarSeccion(id) {
 
 
     
+
 
 
 
